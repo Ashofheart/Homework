49188e4fa9b595551f8b186e7bd96e749e443300
Ashofheart <161328810+Ashofheart@users.noreply.github.com>
2024-03-01 16:58:35 +0300

    coment

---
+++ 13.02.2024(1)/13.02.2024(1)/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Program
+{
+    class Console1
+    {
+        public static void Main()
+        {
+            Ancet();
+            IMT();
+            PointsMod();
+            ChangeInts();
+            SayInform();
+        }
+        public static void Ancet()
+        {
+            string name = Console.ReadLine();
+            string surname = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            int weight = int.Parse(Console.ReadLine());
+            Console.WriteLine("My name is " + name + " " + surname + ", I\'m " + age + " years old. My height is " + height + ", my weight is " + weight + "!\n");
+            Console.WriteLine("My name is {0} {1}, I\'m {2} years old, my height is {3} and weight is {4}!\n", name, surname, age.ToString(), height.ToString(), weight.ToString());
+            Console.WriteLine($"My name is {name} {surname}, I\'m {age.ToString()} years old, my height is {height.ToString()} and weight is {weight.ToString()}!");
+        }
+        public static void IMT()
+        {
+            int w = int.Parse(Console.ReadLine());
+            int h = int.Parse(Console.ReadLine());
+            Console.WriteLine(((float)w / h / h).ToString());
+        }
+        public static void PointsMod()
+        {
+            int x1 = int.Parse(Console.ReadLine()), y1 = int.Parse(Console.ReadLine()), x2 = int.Parse(Console.ReadLine()), y2 = int.Parse(Console.ReadLine());
+            double r = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine(r);
+        }
+        public static void ChangeInts()
+        {
+            int a = int.Parse(Console.ReadLine()), b = int.Parse(Console.ReadLine());
+            a *= b;
+            b = a / b;
+            a /= b;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+        public static void SayInform()
+        {
+            string s = " ";
+            for (int i = 0; i < Console.WindowWidth; i++)
+            {
+                s += " ";
+            }
+            Console.WriteLine("Ivan");
+            Console.WriteLine("Baturin");
+            Console.WriteLine("Kazan");
+            Console.WriteLine(s.Substring(0, Console.WindowWidth / 2 - 2) + "Ivan" + s.Substring(0, Console.WindowWidth / 2 - 2));
+            Console.WriteLine(s.Substring(0, Console.WindowWidth / 2 - 3) + "Baturin" + s.Substring(0, Console.WindowWidth / 2 - 4));
+            Console.WriteLine(s.Substring(0, Console.WindowWidth / 2 - 2) + "Kazan" + s.Substring(0, Console.WindowWidth / 2 - 1));
+        }
+    }
+}
---
+++ 27.02.2024(1)/27.02.2024(1)/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Diagnostics.SymbolStore;
+using System.Runtime.Serialization.Formatters;
+using static System.Net.WebRequestMethods;
+
+namespace Programm
+{
+    class Console1
+    {
+        public static void Main()
+        {
+            Console.WriteLine(Min_three());
+            Console.WriteLine(ncnt());
+            Console.WriteLine(SumNch());
+            Examination();
+            Console.WriteLine(IMT());
+            OtADoB();
+        }
+        public static int Min_three()
+        {
+            int n1 = int.Parse(Console.ReadLine()), n2 = int.Parse(Console.ReadLine()), n3 = int.Parse(Console.ReadLine());
+            return Math.Min(Math.Min(n1, n2), n3);
+        }
+        public static int ncnt()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int cnt = 0;
+            while (n != 0)
+            {
+                n /= 10;
+                cnt++;
+            }
+            return cnt;
+        }
+        public static int SumNch()
+        {
+            int a;
+            int su = 0;
+            while (true)
+            {
+                a = int.Parse(Console.ReadLine());
+                if (a == 0)
+                {
+                    return su;
+                }
+                if (a % 2 == 1 && a > 0)
+                {
+                    su += a;
+                }
+            }
+        }
+        public static bool CheckProf(string log, string pas)
+        {
+            return log == "root" && pas == "ITTop";
+        }
+        public static void Examination()
+        {
+            string log;
+            string pas;
+            int cnt = 1;
+            bool pass = false;
+            do
+            {
+                log = Console.ReadLine();
+                pas = Console.ReadLine();
+                if (CheckProf(log, pas))
+                {
+                    pass = true;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Login or password is not right!");
+                }
+                cnt++;
+            } while (cnt <= 3);
+            if (pass)
+            {
+                Console.WriteLine("You passed examination!");
+            }
+            else
+            {
+                Console.WriteLine("You failed examination!");
+            }
+        }
+        public static int IMT()
+        {
+            int h = int.Parse(Console.ReadLine());
+            int w = int.Parse(Console.ReadLine());
+            double ans = w / Math.Pow(h / 100.0, 2);
+            int ans1 = (int)Math.Round(ans, 0);
+            if (ans1 < 22)
+            {
+                Console.WriteLine("You need to eat more!");
+            }
+            else if (ans1 > 22)
+            {
+                Console.WriteLine("You need to do sport!");
+            }
+            return ans1;
+        }
+        public static void OtADoB(int a = -1, int b = -1)
+        {
+            if (a == -1 && b == -1)
+            {
+                a = int.Parse(Console.ReadLine());
+                b = int.Parse(Console.ReadLine());
+            }
+            if (a < b)
+            {
+                Console.WriteLine(a);
+                OtADoB(a + 1, b);
+            }
+            else if (a == b)
+            {
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
